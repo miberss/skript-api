1032ef62c955f83480f614cca900d0f7b78ac54b
mibers <woodrow.conran@gmail.com>
2025-12-26 19:22:18 +0000

    fix please?

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import index from "./index.html"
 const port = process.env.PORT || 3000;
 
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("UNHANDLED REJECTION:", reason);
+});
+
 const server = Bun.serve({
   port: port,
   routes: {
